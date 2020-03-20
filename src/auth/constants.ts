9ae038257f98fcb4ab298feceed2e2ee1cd1bfb9
LiFu-ChrisLee <753641657@qq.com>
2020-03-20 17:27:48 +0800

    use passport local and jwt

---
+++ src/auth/constants.ts
@@ -0,0 +1,9 @@
+/**
+ * @author LiFu
+ * @date 2020/3/20 17:08
+ * @description secret key
+ */
+
+const jwtConstants = { secret: 'secretKey' };
+
+export { jwtConstants };
---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,25 @@
+/**
+ * @author LiFu
+ * @date 2020/3/20 17:07
+ * @description jwt
+ */
+
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import { jwtConstants } from './constants';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: jwtConstants.secret,
+    });
+  }
+
+  async validate(payload: any) {
+    return { userId: payload.sub, username: payload.username };
+  }
+}
